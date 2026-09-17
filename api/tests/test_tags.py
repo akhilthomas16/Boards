b97e10c4903ba89b9a6c092886687acf8971fc98
akhilthomas16 <akhilthomasthazhatha@gmail.com>
2026-09-17 12:34:52 +0530

    fix: remove stale duplicate app from api package

--- api/tests/test_tags.py
+++ api/tests/test_tags.py
@@ -3,8 +3,13 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-from api.routers.topics import _normalize_tags
+import django  # noqa: E402
+
+django.setup()
+
+from api.routers.topics import _normalize_tags  # noqa: E402
 
 
 def test_normalize_tags():
